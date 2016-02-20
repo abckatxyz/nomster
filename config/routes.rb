3fc8e411b1732e86c3e377758c48d5c40f6ac05f
Kat Maribao <kmaribao@gmail.com>
2016-02-20 20:17:07 +0000

    submit button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Nomster::Application.routes.draw do
 
   devise_for :users
   resources :places
+  resources :places do
+    resources :comments, only: :create
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
