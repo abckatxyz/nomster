060e8ce411ca160fdba79e1e6aa577aa8a4dcafd
Kat Maribao <kmaribao@gmail.com>
2016-03-03 00:57:53 +0000

    redo carrierwave migration

--- db/migrate/20160302235617_add_picture_to_photos.rb
+++ db/migrate/20160302235617_add_picture_to_photos.rb
@@ -1,5 +1,5 @@
 class AddPictureToPhotos < ActiveRecord::Migration
   def change
-    add_column :photos, :avatar, :string
+    add_column :photos, :picture, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223044623) do
+ActiveRecord::Schema.define(version: 20160302235617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20160223044623) do
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar"
+    t.string   "picture"
   end
 
   create_table "places", force: true do |t|
