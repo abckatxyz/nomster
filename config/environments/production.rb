ef2bae6cf9aaf1fa44d4a6057eae311b575b6619
Kat Maribao <kmaribao@gmail.com>
2016-03-04 01:12:06 +0000

    MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 Nomster::Application.configure do
   config.action_mailer.default_url_options = { host: 'nomster-kat-maribao.herokuapp.com/' }
+
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'nomster-kat-maribao.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
